fba17f78e3df7296f1bdf8397f534d87677178df
Ayush dubey <103594073+ayushdubes@users.noreply.github.com>
2024-06-06 12:43:13 +0000

    errorhandler added along with other controller functions

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -19,11 +19,11 @@ export const getUser = async (req, res, next) =>{
       const id = req.params.id;
       console.log(`Received ID: ${id}`); // Logging the received ID
       
-      const query = {_id: new ObjectId(req.params.id)};
+      const query = {_id:new ObjectId(req.params.id)};
       const user = await collection.findOne(query);
 
       if(!user){
-        return res.status(404).json({message: 'user not found'});
+        return next({status:404,message:'user not found'});
       }
     res.status(200).json(user);
     } catch (error){
@@ -34,3 +34,55 @@ export const getUser = async (req, res, next) =>{
 };
 
 // updateUser function                        
+export const updateUser = async (req, res, next) =>{
+  try{
+    //check if password is present and then hash it using salt
+    if(req.body.password){
+      req.body.password = await bcrypt.hash(req.body.password,10);
+    }
+    //find out the query - which user to update
+    const query = {_id: new ObjectId(req.params.id)}
+
+    // set the data to be updated
+    const data = {
+      $set: {
+        ...req.body,
+        updatedAt: new Date().toISOString()
+      },
+    }
+    // option of when to retirn the updated document
+
+    const options = {
+      returnDocument: 'after',
+    }
+    // update the document
+
+    const updatedUser = await collection.findOneAndUpdate(query,data,options);
+
+    //spread the document entries and hide sensitive information
+
+    const{password:pass, updatedAt, createdAt, ...rest} = uupdatedUser;
+    res.status(200).json(updatedUser);
+    
+  } catch(error){
+    next({status:500, error});
+  } 
+}
+
+
+export const deleteUser = async(req,res,next) =>{
+  try{
+    //find the user to be deleted
+    const query = {_id: new ObjectId(req.params.id)};
+    const user = await collection.findOne(query);
+
+    if(!user){
+      return res.status(404).json({message: 'user not found'});
+    }
+    //delete the user
+    await collection.deleteOne(query);
+    res.status(200).json({message: 'user deleted'});
+  } catch(error){
+    next({status:500, error});
+  }
+}
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import 'dotenv/config';
 import { db } from './libs/dbConnect.js';
 import userRouter from './routes/user.route.js';
+import {errorHandler} from './libs/middleware.js';
 
 const app = express();
 const PORT = 3000;
@@ -11,6 +12,7 @@ app.use('*' , (req, res) => {
   res.status(404).json({message: 'not found'});
 });
 
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
